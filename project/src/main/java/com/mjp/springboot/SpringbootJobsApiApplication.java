e0632f1bbc75d3f64b67351a4b2f809a9e0c5de5
marnusKriek <109765436+marnusKriek@users.noreply.github.com>
2022-07-22 00:31:16 +0200

    Add files via upload

---
+++ project/src/main/java/com/mjp/springboot/SpringbootJobsApiApplication.java
@@ -0,0 +1,13 @@
+package com.mjp.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJobsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJobsApiApplication.class, args);
+	}
+
+}
